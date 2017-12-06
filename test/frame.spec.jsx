84377c9db973fedbcec4cc1e75234d9e383c8da6
eugeneford <stmechanus@gmail.com>
2017-12-06 12:02:13 +0200

    add frame tests

--- test/frame.spec.jsx
+++ test/frame.spec.jsx
@@ -1,8 +1,88 @@
-import { mount } from 'enzyme';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Frame from '../src/frame';
 
-describe('Frame', function () {
-  it('is defined', function () {
-    expect(Frame).toBeDefined();
+describe('Frame', () => {
+  const parser = new DOMParser();
+  let root;
+
+  beforeEach(()=>{
+    root = document.createElement('div');
+    root.setAttribute('id', 'root');
+    document.body.appendChild(root);
+  });
+
+  afterEach(()=>{
+    document.body.removeChild(root);
+  });
+
+  it('dom is rendered', ()=> {
+    const html = '<h1>Hello World</h1>';
+    const dom = parser.parseFromString(html, 'text/html');
+
+    ReactDOM.render(<Frame src={dom}/>, document.getElementById('root'));
+
+    const iframe = document.getElementById('syncy-frame-instance');
+    const frameWindow = iframe.contentWindow;
+    const frameDocument = frameWindow.document;
+
+    expect(frameDocument.body.innerHTML).toEqual(html);
+  });
+
+  it('didnt update with the same src', ()=> {
+    const html = '<h1>Hello World</h1>';
+    const dom = parser.parseFromString(html, 'text/html');
+    const spy = jasmine.createSpy();
+
+    ReactDOM.render(<Frame src={dom} onBeforeLoad={spy}/>, document.getElementById('root'));
+    ReactDOM.render(<Frame src={dom} onBeforeLoad={spy}/>, document.getElementById('root'));
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('custom id is applied', ()=> {
+    const html = '<h1>Hello World</h1>';
+    const dom = parser.parseFromString(html, 'text/html');
+
+    ReactDOM.render(<Frame id={'custom-frame'} src={dom}/>, document.getElementById('root'));
+
+    const iframe = document.getElementById('custom-frame');
+
+    expect(iframe).toBeDefined();
+  });
+
+  it('onBeforeLoad is called', ()=> {
+    const html = '<h1>Hello World</h1>';
+    const dom = parser.parseFromString(html, 'text/html');
+    const spy = jasmine.createSpy();
+
+    ReactDOM.render(<Frame src={dom} onBeforeLoad={spy}/>, document.getElementById('root'));
+
+    const iframe = document.getElementById('syncy-frame-instance');
+
+    expect(spy).toHaveBeenCalledWith(iframe);
+  });
+
+  it('onLoad is called', ()=> {
+    const html = '<h1>Hello World</h1>';
+    const dom = parser.parseFromString(html, 'text/html');
+    const spy = jasmine.createSpy();
+
+    ReactDOM.render(<Frame src={dom} onLoad={spy}/>, document.getElementById('root'));
+
+    const iframe = document.getElementById('syncy-frame-instance');
+
+    expect(spy).toHaveBeenCalledWith(iframe);
+  });
+
+  it('url is rendered', (done)=> {
+    const url = 'http://default.eugeneford.info/';
+    const testLocation = (iframe) => {
+      const { src } = iframe;
+      expect(src).toEqual(url);
+      done();
+    };
+
+    ReactDOM.render(<Frame src={url} onLoad={testLocation}/>, document.getElementById('root'));
   });
 });
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,25 @@
-import './frame.spec.jsx';
-import './syncy-frame.spec.jsx';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-import SyncyFrame from '../src/index';
+Enzyme.configure({ adapter: new Adapter() });
 
-describe('SyncyFrame', function () {
-  it('is defined', function () {
+import Frame from '../src/frame';
+import SyncyFrame from '../src/syncy-frame';
+import ReactSyncyFrame from '../src/index';
+
+describe('index', function () {
+  it('ReactSyncyFrame is defined', function () {
+    expect(ReactSyncyFrame).toBeDefined();
+  });
+
+  it('SyncyFrame is defined', () => {
     expect(SyncyFrame).toBeDefined();
   });
+
+  it('Frame is defined', () => {
+    expect(Frame).toBeDefined();
+  });
 });
+
+import './frame.spec.jsx';
+import './syncy-frame.spec.jsx';
